82dd620dbcabfa3d2c7aca13b39366d544d594ab
unknown <Administrator@CCF-140.www.fetccf.com>
2014-05-19 11:16:29 +0530

    aplha

---
+++ new/train.py
@@ -0,0 +1,44 @@
+import json,re
+fi=open('train-exp1.svm', 'r')
+model = {}
+labels_occ = {}
+c  = 0
+while True:
+	line = fi.readline()
+	if not line:
+		break
+	parts = line.split()
+	if len(parts) < 3:
+		continue
+	if re.findall(r'\d+:\d+\.\d+', str(parts[0])) != []:
+		continue
+	labels, features = parts[0].split(','), parts[1:]
+	for label in labels:
+		if not label in labels_occ.keys():
+			labels_occ[label] = {}
+
+		if not label in model.keys():
+			model[label] = {}
+		for feature in features:
+			f,w = feature.split(':')
+			try:
+				model[label][f] += float(w)
+			except:
+				model[label][f] = float(w)
+			try:
+				labels_occ[label][f] += 1
+			except:
+				labels_occ[label][f] = 1
+
+	if c % 1000 == 0: print c
+	c += 1
+
+for k,v in model.iteritems():
+	for k1,v1 in v.iteritems():
+		model[k][k1] /= labels_occ[k][k1]
+
+fi.close()
+f=open('model.json', 'w')
+json.dump(model, f)
+f.close()
+
